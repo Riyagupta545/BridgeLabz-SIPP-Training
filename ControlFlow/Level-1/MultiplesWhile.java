c5578142786619a9dc7923d95808adb8a7964c0c
Rini-Gupta1 <riniguptajhansi2005@gmail.com>
2025-06-16 11:36:49 +0530

    [Riya gupta] added multiples using while loop level 2

---
+++ ControlFlow/Level-1/MultiplesWhile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MultiplesWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number less than 100: ");
+        int number = sc.nextInt();
+
+        if (number <= 0 || number >= 100) {
+            System.out.println("Enter a valid number between 1 and 99.");
+            return;
+        }
+
+        int counter = 100;
+        while (counter > 1) {
+            if (counter % number == 0) {
+                System.out.println(counter);
+            }
+            counter--;
+        }
+    }
+}
