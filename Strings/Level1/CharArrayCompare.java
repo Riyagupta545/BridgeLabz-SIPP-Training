b1484409a38ed93c168a0ce635c8cd649b022232
Rini-Gupta1 <riniguptajhansi2005@gmail.com>
2025-06-20 08:51:29 +0530

    [Riya Gupta] added Return Characters without toCharArray() program

---
+++ Strings/Level1/CharArrayCompare.java
@@ -0,0 +1,34 @@
+package Strings.Level1;
+import java.util.Scanner;
+
+public class CharArrayCompare {
+    public static char[] manualToCharArray(String str) {
+        char[] arr = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.charAt(i);
+        }
+        return arr;
+    }
+
+    public static boolean compareArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String text = sc.next();
+        sc.close();
+
+        char[] manual = manualToCharArray(text);
+        char[] builtin = text.toCharArray();
+
+        System.out.println("Arrays match: " + compareArrays(manual, builtin));
+    }
+}
+
+
