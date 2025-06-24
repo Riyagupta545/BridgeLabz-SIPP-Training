2e9cfb42f16521ff9172698a4c699ed622b8f5ba
Rini-Gupta1 <riniguptajhansi2005@gmail.com>
2025-06-24 08:26:49 +0530

    [Riya Gupta] added Fibonacci Generator program

---
+++ BuiltInFunction/Practice1/FibonacciGenerator.java
@@ -0,0 +1,24 @@
+package BuiltInFunction.Practice1;
+import java.util.Scanner;
+
+public class FibonacciGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        printFibonacci(n);
+        sc.close();
+    }
+
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Sequence: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+}
